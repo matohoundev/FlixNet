66709b3c306b92dd489b0754da64533246bd2c64
matohoundev <matohoundev@gmail.com>
2023-09-04 19:53:27 +0200

    Banner end

---
+++ components/banner/movie.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-[80vh] relative">
+    <div
+      class="w-full h-[80vh] absolute flex items-center justify-center md:justify-normal px-12 md:px-32"
+    >
+      <p class="z-10 text-xl md:text-6xl text-white font-extrabold">
+        Spider-Man : Across the Spider-Verse
+      </p>
+    </div>
+    <img
+      src="@/assets/img/spider.jpg"
+      class="w-full h-full object-cover object-top"
+      alt="Spider-Man : Across the Spider-Verse"
+    />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <p>Start</p>
+    <BannerMovie />
   </div>
 </template>
