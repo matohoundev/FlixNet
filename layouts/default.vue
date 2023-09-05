19b5000abc3338f88bc488e152a20e7597934a11
matohoundev <matohoundev@gmail.com>
2023-09-05 19:53:51 +0200

    add storage for the end

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import { useFavoritesStore } from "@/stores/favorites";
+const favorites = useFavoritesStore();
+
+onMounted(() => {
+  favorites.init();
+});
+</script>
+
 <template>
   <div class="bg-rose-900">
     <StarterHeader />
--- stores/favorites.ts
+++ stores/favorites.ts
@@ -8,11 +8,19 @@ export const useFavoritesStore = defineStore("favorites", {
     }[],
   }),
   actions: {
+    init() {
+      const movies = localStorage.getItem("favorites movies");
+      if (movies) {
+        this.movies = JSON.parse(movies);
+      }
+    },
     add(id: number, poster_path: string) {
       this.movies.push({ id, poster_path });
+      localStorage.setItem("favorites movies", JSON.stringify(this.movies));
     },
     remove(id: number) {
       this.movies = this.movies.filter((movie) => movie.id !== id);
+      localStorage.setItem("favorites movies", JSON.stringify(this.movies));
     },
   },
 });
